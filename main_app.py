90cb8552c8070fee68fe61fa86f2158cd18c92a9
David Teixeira <127703313+Teixeira-David@users.noreply.github.com>
2024-03-29 15:53:55 -0400

    First Commit With Standard Info

---
+++ main_app.py
@@ -0,0 +1,38 @@
+"""
+Project Name: Password Manager Tool
+Developer: David Teixeira, Kara Jacobs, Jennifer Dillehay
+Date: 03/28/2024
+Abstract: This project is vol 0.0.1 for SDEV-265 Final Project. Please refer to the GitHub repository 
+for the most up-to-date version.
+
+    Repository: https://github.com/Teixeira-David/SDEV-265-PasswordManagerTool.git
+    
+    
+    File Abstract: This file is the main entry point for the SDEV-265 Password Manager Tool.
+"""
+
+# Import Python Libraries
+
+# Import project modules
+
+
+
+#######################################################################################################
+# Main Start of Program
+#######################################################################################################         
+
+def Main():
+    """ 
+    Function Name: Main Start Program
+    Function Description: This function begins the program. Open the GUI launch Page
+    """
+    try:
+        # Start the main GUI window  
+        
+    # Display error message if the entry is invalid
+    except Exception as err:
+        print("Exception occurred because", err)
+
+# This is the start of main program
+if __name__ == '__main__':
+    Main()
